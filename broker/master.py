dba04bfaf0e7e8e898273205c27fc874b06cf167
Mateusz Kadula <mateusz.kadula@icloud.com>
2023-04-17 14:23:32 +0200

    send and await submit for Submit class #862jk2hcp [IN PROGRESS]
    plus some minor changes in HTTP server

--- broker/master.py
+++ broker/master.py
@@ -11,13 +11,19 @@ class BrokerMaster:
                  db_string: str,
                  results_dir: Path,
                  delete_records: bool = True,
-                 threads: int = 2
+                 threads: int = 2,
+                 server_address: tuple[str, int] = ('127.0.0.1', 8080)
                  ):
         self.connection = Connection(db_string)
         self.delete_records = delete_records
         self.results_dir = results_dir
         self.threads = threads
         self.submits = {}
+        self.submit_http_server = KolejkaCommunicationServer(*server_address)
+        self.submit_http_server.start_server()
+
+    def __del__(self):
+        self.submit_http_server.stop_server()
 
     def new_submit(self,
                    submit_id: str,
@@ -47,12 +53,15 @@ class KolejkaCommunicationServer:
 
     def stop_server(self) -> None:
         self.server.shutdown()
+        self.server.server_close()
 
     def is_active(self) -> bool:
         return self.server_thread.is_alive()
 
     def add_submit(self, submit_id: str) -> None:
         with self.integrity_lock:
+            if submit_id in self.submit_dict:
+                raise ValueError('Submit with id %s already registered.' % submit_id)
             self.submit_dict[submit_id] = Lock()
             self.submit_dict[submit_id].acquire()
 
@@ -61,14 +70,16 @@ class KolejkaCommunicationServer:
             if self.submit_dict[submit_id].locked():
                 self.submit_dict[submit_id].release()
             else:
-                raise ValueError  # TODO
+                raise ValueError('Submit with id %s has already been released.' % submit_id)
 
-    def await_submit(self, submit_id: str) -> None:
+    def await_submit(self, submit_id: str, timeout: float = -1) -> bool:
         with self.integrity_lock:
             lock = self.submit_dict[submit_id]
-        lock.acquire()
-        with self.integrity_lock:
-            del self.submit_dict[submit_id]
+        lock_acquired = lock.acquire(timeout=timeout)
+        if lock_acquired:
+            with self.integrity_lock:
+                del self.submit_dict[submit_id]
+        return lock_acquired
 
 
 class ThreadingHTTPServer2(ThreadingHTTPServer):
@@ -85,17 +96,23 @@ class ThreadingHTTPServer2(ThreadingHTTPServer):
 
 class _KolejkaCommunicationHandler(BaseHTTPRequestHandler):
 
+    def __init__(self, request: bytes, client_address: tuple[str, int], server: ThreadingHTTPServer2):
+        super().__init__(request, client_address, server)
+        self.server: ThreadingHTTPServer2 = server
+
     def do_GET(self):  # TODO rewrite this entire method
         """
         Handles http requests
         """
         manager: KolejkaCommunicationServer = self.server.manager
-        submit_id = ''.join(filter(lambda x: x != '/', self.path))  # TODO
-        self.send_response(200)
-        self.end_headers()
+        submit_id = ''.join(filter(lambda x: x != '/', self.path))
         try:
             manager.release_submit(submit_id)
         except (KeyError, ValueError):
+            self.send_response(404)
+            self.end_headers()
             self.wfile.write(b'F')
         else:
+            self.send_response(200)
+            self.end_headers()
             self.wfile.write(b'S')
