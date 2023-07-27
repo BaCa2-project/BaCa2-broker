c21c46e6d82ececc0b2ef86fa23256575baf95fd
Bartosz Deptuła <barti.deptula@gmail.com>
2023-07-27 17:14:18 +0200

    BRK-12 Package translation sceleton

--- broker/builder.py
+++ broker/builder.py
@@ -1,1 +1,44 @@
-#TODO
+from baca2PackageManager import Package
+from settings import BUILD_NAMESPACE
+
+
+class Builder:
+    def __init__(self, package: Package) -> None:
+        self.package = package
+        self.build_namespace = BUILD_NAMESPACE
+        self.build_path = None
+
+    @property
+    def is_built(self) -> bool:
+        return self.package.check_build(self.build_namespace)
+
+    def _generate_test_yaml(self):
+        test_yaml = {
+            'memory': '512MB',
+            'kolejka': {
+                'image': 'kolejka/satori:judge',
+                'exclusive': False,
+                'requires': ['cpu:xeon e3-1270 v5'],
+                'collect': ['log.zip'],
+                'limits': {
+                    'time': '600s',
+                    'memory': '10G',
+                    'swap': 0,
+                    'cpus': self.package['cpus'],
+                    'network': self.package['network'],
+                    'storage': '5G',
+                    'workspace': '5G',
+                }
+            }
+        }
+
+        return test_yaml
+
+    def build(self):
+        self.build_path = self.package.prepare_build(self.build_namespace)
+
+        test_yaml = self._generate_test_yaml()
+
+        # TODO
+
+
--- settings.py
+++ settings.py
@@ -1,10 +1,16 @@
 # Settings for broker
 from pathlib import Path
+from baca2PackageManager import set_base_dir, add_supported_extensions
 
 BASE_DIR = Path(__file__).resolve().parent
+BACA2_DIR = BASE_DIR.parent.parent / 'BaCa2'  # Change if you have a different path
+PACKAGES_DIR = BACA2_DIR / 'packages_source'
+
 DB_STRING = f"sqlite://{BASE_DIR.absolute()}/submit_control.db"
 
 DELETE_RECORDS = False
 
+BUILD_NAMESPACE = 'kolejka'
 
-
+set_base_dir(PACKAGES_DIR)
+add_supported_extensions('cpp')
