5ab54b01443924f7636049125dfc1c2756f8d6f4
Bartosz Deptuła <barti.deptula@gmail.com>
2023-12-17 17:54:16 +0100

    Package app update: - added models managers, - added tests for package actions - added package instance user workflow - added zip unpacking into package instance / package source

--- scratch.py
+++ scratch.py
@@ -8,6 +8,7 @@ pkg_path = BASE_DIR / 'tests' / 'test_packages' / '1'
 submit_path = pkg_path / '1' / 'prog' / 'solution.cpp'
 
 master = BrokerMaster(DB_STRING, SUBMITS_DIR, APP_SETTINGS['delete_records'])
+master.start()
 master.connection.truncate_db()
 
 master.new_submit(
